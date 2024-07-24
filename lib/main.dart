950d6a76ffc43f3ff7619c68c29a713e9c201e26
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-07-24 16:05:21 +0700

    widget dasar

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,61 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green,
-        body: Center(
-          child: Text("Hello World"),
+        appBar: AppBar(
+          title: const Text("Flutter Basic"),
+          centerTitle: true,
+          backgroundColor: Colors.red[900],
         ),
+        body: Center(
+          child: Image.asset("images/image.jpg"),
+        ),
+        // body: const Center(
+        //   child: Image(
+        //     image: NetworkImage(
+        //         "https://docs.flutter.dev/assets/images/dash/dash-fainting.gif"),
+        //   ),
+        // ),
+        // body: const Center(
+        //   child: Image(
+        //     image: AssetImage("images/image.jpg"),
+        //   ),
+        // ),
+        // body: Center(
+        //   child: Icon(
+        //     Icons.home,
+        //     size: 100,
+        //     color: Colors.red[900],
+        //   ),
+        // ),
+        // body: Center(
+        //   child: ElevatedButton(
+        //     onPressed: () {
+        //       print("Klik saya");
+        //     },
+        //     child: const Text("Ini Button saya"),
+        //   ),
+        // ),
+        // body: const Center(
+        //   child: FlutterLogo(
+        //     size: 100,
+        //   ),
+        // ),
+        // body: Center(
+        //   child: Container(
+        //     width: 200,
+        //     height: 200,
+        //     color: Colors.green,
+        //     child: const Center(
+        //       child: Text("Widget Dasar"),
+        //     ),
+        //   ),
+        // ),
       ),
     );
   }
