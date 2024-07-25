363ea59a3f742a4ad53810c0fa1797108260d4d5
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-07-25 21:09:27 +0700

    widget builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,51 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/widgets/color_box.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  // List<ColorBox> allItems = List.generate(
-  //   10,
-  //   (index) => ColorBox(
-  //     text: "Box - ${index + 1}",
-  //     colors: Color.fromARGB(
-  //       255,
-  //       200 + Random().nextInt(256),
-  //       200 + Random().nextInt(256),
-  //       200 + Random().nextInt(256),
-  //     ),
-  //   ),
-  // );
-
-  List<Map<String, dynamic>> data = List.generate(
-    10,
-    (index) => {
-      "text": "Box - ${index + 1}",
-      "color": Color.fromARGB(
-        255,
-        200 + Random().nextInt(256),
-        200 + Random().nextInt(256),
-        200 + Random().nextInt(256),
-      ),
-    },
-  );
-
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Extra Widget"),
+          title: const Text("Widget Builder"),
           centerTitle: true,
-          backgroundColor: Colors.red[900],
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: data
-                .map(
-                  (e) => ColorBox(
-                    text: e["text"],
-                    colors: e["color"],
-                  ),
-                )
-                .toList(),
+        body: GridView.builder(
+          padding: const EdgeInsets.all(10),
+          itemCount: 50,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 5,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemBuilder: (context, index) => Container(
+            color: Color.fromARGB(
+              255,
+              60 + Random().nextInt(151),
+              60 + Random().nextInt(151),
+              60 + Random().nextInt(151),
+            ),
           ),
         ),
+        // body: ListView.builder(
+        //   itemCount: 10,
+        //   itemBuilder: (context, index) => ColorBox(
+        //     text: "What is the Box - ${index + 1}",
+        //     colors: Color.fromARGB(
+        //       255,
+        //       60 + Random().nextInt(200),
+        //       60 + Random().nextInt(200),
+        //       60 + Random().nextInt(200),
+        //     ),
+        //   ),
+        // ),
       ),
     );
   }
