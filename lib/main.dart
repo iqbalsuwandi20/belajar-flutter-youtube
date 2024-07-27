7161f45d5f6c7a2d2775c1e659df9711a7887367
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-07-27 19:31:13 +0700

    stateless vs stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,154 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/widgets/question_twentyfour.dart';
-// import 'package:flutter_basic/widgets/question_twentytree.dart';
-// import 'package:flutter_basic/widgets/question_twentytwo.dart';
-// import 'package:flutter_basic/widgets/question_twentyone.dart';
-// import 'package:flutter_basic/widgets/question_twenty.dart';
-// import 'package:flutter_basic/widgets/question_nineteen.dart';
-// import 'package:flutter_basic/widgets/question_eighteen.dart';
-// import 'package:flutter_basic/widgets/question_seventeen.dart';
-// import 'package:flutter_basic/widgets/question_sixteen.dart';
-// import 'package:flutter_basic/widgets/question_fiveteen.dart';
-// import 'package:flutter_basic/widgets/question_fourteen.dart';
-// import 'package:flutter_basic/widgets/question_thirteen.dart';
-// import 'package:flutter_basic/widgets/question_eleven.dart';
-// import 'package:flutter_basic/widgets/question_twelve.dart';
-// import 'package:flutter_basic/widgets/question_nine.dart';
-// import 'package:flutter_basic/widgets/question_ten.dart';
-// import 'package:flutter_basic/widgets/question_eight.dart';
-// import 'package:flutter_basic/widgets/question_seven.dart';
-// import 'package:flutter_basic/widgets/question_five.dart';
-// import 'package:flutter_basic/widgets/question_six.dart';
-// import 'package:flutter_basic/widgets/question_four.dart';
-// import 'package:flutter_basic/widgets/question_tree.dart';
-// import 'package:flutter_basic/widgets/question_one.dart';
-// import 'package:flutter_basic/widgets/question_two.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int value = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: QuestionTwentyfour(),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red[900],
+          title: const Text(
+            "Stateless vs Stateful",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                value.toString(),
+                style: const TextStyle(
+                  fontSize: 50,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      value = value - 1;
+                      print(value);
+                      setState(() {});
+                    },
+                    child: Icon(
+                      Icons.remove,
+                      color: Colors.red[900],
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      value = value + 1;
+                      print(value);
+                      setState(() {});
+                    },
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.red[900],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
+/**
+ * STATELESS
+ */
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// // ignore: must_be_immutable
+// class MyApp extends StatelessWidget {
+//   MyApp({super.key});
+
+//   int value = 0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.red[900],
+//           title: const Text(
+//             "Stateless vs Statefull",
+//             style: TextStyle(
+//               color: Colors.white,
+//             ),
+//           ),
+//           centerTitle: true,
+//         ),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text(
+//                 value.toString(),
+//                 style: const TextStyle(
+//                   fontSize: 50,
+//                 ),
+//               ),
+//               const SizedBox(
+//                 height: 20,
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                 children: [
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       value = value - 1;
+//                       print(value);
+//                     },
+//                     child: Icon(
+//                       Icons.remove,
+//                       color: Colors.red[900],
+//                     ),
+//                   ),
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       value = value + 1;
+//                       print(value);
+//                     },
+//                     child: Icon(
+//                       Icons.add,
+//                       color: Colors.red[900],
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
