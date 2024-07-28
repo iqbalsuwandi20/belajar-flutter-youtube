590e7358344bd5419864059f8a525c2c128e6cdf
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-07-28 16:10:26 +0700

    dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,154 +1,69 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int value = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red[900],
-          title: const Text(
-            "Stateless vs Stateful",
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                value.toString(),
-                style: const TextStyle(
-                  fontSize: 50,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dialog"),
+        centerTitle: true,
+        backgroundColor: Colors.red[900],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
+                title: const Text(
+                  "This is a dialog title",
+                ),
+                content: const Text(
+                  "This is the description of the dialog, you can see it here",
+                ),
+                actions: [
                   ElevatedButton(
-                    onPressed: () {
-                      value = value - 1;
-                      print(value);
-                      setState(() {});
-                    },
-                    child: Icon(
-                      Icons.remove,
-                      color: Colors.red[900],
+                    onPressed: () {},
+                    child: const Text(
+                      "Okay, I got it",
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: () {
-                      value = value + 1;
-                      print(value);
-                      setState(() {});
-                    },
-                    child: Icon(
-                      Icons.add,
-                      color: Colors.red[900],
+                    onPressed: () {},
+                    child: const Text(
+                      "Cancel",
                     ),
                   ),
                 ],
               ),
-            ],
-          ),
+            );
+          },
+          child: const Text("Show Dialog"),
         ),
       ),
     );
   }
 }
-
-/**
- * STATELESS
- */
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// // ignore: must_be_immutable
-// class MyApp extends StatelessWidget {
-//   MyApp({super.key});
-
-//   int value = 0;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: Colors.red[900],
-//           title: const Text(
-//             "Stateless vs Statefull",
-//             style: TextStyle(
-//               color: Colors.white,
-//             ),
-//           ),
-//           centerTitle: true,
-//         ),
-//         body: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               Text(
-//                 value.toString(),
-//                 style: const TextStyle(
-//                   fontSize: 50,
-//                 ),
-//               ),
-//               const SizedBox(
-//                 height: 20,
-//               ),
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                 children: [
-//                   ElevatedButton(
-//                     onPressed: () {
-//                       value = value - 1;
-//                       print(value);
-//                     },
-//                     child: Icon(
-//                       Icons.remove,
-//                       color: Colors.red[900],
-//                     ),
-//                   ),
-//                   ElevatedButton(
-//                     onPressed: () {
-//                       value = value + 1;
-//                       print(value);
-//                     },
-//                     child: Icon(
-//                       Icons.add,
-//                       color: Colors.red[900],
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
