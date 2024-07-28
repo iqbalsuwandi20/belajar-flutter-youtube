635d9f534fa438ababd941012393a7f9258ac2f2
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-07-28 16:28:58 +0700

    snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
@@ -17,51 +17,50 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({
-    super.key,
-  });
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Dialog"),
+        title: const Text("SNACKBAR"),
         centerTitle: true,
         backgroundColor: Colors.red[900],
       ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                title: const Text(
-                  "This is a dialog title",
-                ),
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
                 content: const Text(
-                  "This is the description of the dialog, you can see it here",
+                  "Changing profile using the snackbar succeeded",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
                 ),
-                actions: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Okay, I got it",
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Cancel",
-                    ),
-                  ),
-                ],
+                action: SnackBarAction(
+                  label: "Cancel",
+                  onPressed: () {
+                    print("Didn't change profile");
+                  },
+                  textColor: Colors.black,
+                ),
+                backgroundColor: Colors.red[900],
+                duration: const Duration(
+                  seconds: 2,
+                ),
+                margin: const EdgeInsets.all(
+                  20,
+                ),
+                behavior: SnackBarBehavior.floating,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
               ),
             );
           },
-          child: const Text("Show Dialog"),
+          child: const Text(
+            "Show Snackbar",
+          ),
         ),
       ),
     );
