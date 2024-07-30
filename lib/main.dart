3d5d84f0dc8c1c3038efd793d1b82e65d368de6e
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-07-30 14:43:48 +0700

    bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -16,33 +16,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late int index;
-
-  List showWidget = [
-    const Center(
-      child: Text("HOME"),
-    ),
-    const Center(
-      child: Text("PROFILE"),
-    ),
-    const Center(
-      child: Text("CART"),
-    ),
-  ];
-
-  @override
-  void initState() {
-    index = 0;
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,36 +26,59 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.red[900],
         centerTitle: true,
         title: const Text(
-          "Bottom Navigation Bar",
+          "Bottom Sheet",
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: showWidget[index],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.red[900],
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey[900],
-        iconSize: 30,
-        currentIndex: index,
-        onTap: (value) {
-          setState(() {
-            index = value;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: ElevatedButton(
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                isDismissible: false,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                builder: (context) => SizedBox(
+                  height: 300,
+                  // color: Colors.white,
+                  child: ListView(
+                    children: [
+                      ListTile(
+                        onTap: () => print("click on photo"),
+                        leading: const Icon(Icons.photo),
+                        title: const Text("Photo"),
+                      ),
+                      ListTile(
+                        onTap: () => print("click on music"),
+                        leading: const Icon(Icons.music_note),
+                        title: const Text("Music"),
+                      ),
+                      ListTile(
+                        onTap: () => print("click on video"),
+                        leading: const Icon(Icons.video_collection),
+                        title: const Text("Video"),
+                      ),
+                      ListTile(
+                        onTap: () => print("click on share"),
+                        leading: const Icon(Icons.share),
+                        title: const Text("Share"),
+                      ),
+                      ListTile(
+                        onTap: () => Navigator.pop(context),
+                        leading: const Icon(Icons.cancel),
+                        title: const Text("Cancel"),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+            child: const Text("Show Bottom Sheet"),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Profile",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: "Cart",
-          ),
-        ],
+        ),
       ),
     );
   }
