c3e089a52866a61f7c8e735f64e8c78684621c39
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-07-31 11:02:59 +0700

    learning instagram UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/pages/home_page.dart';
+import 'package:flutter_basic/pages/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: ProfilePage(),
     );
   }
 }
