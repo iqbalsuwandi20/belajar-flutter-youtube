b104dcba5cd60597f7a356d63dd5bb65982b784f
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-08-03 12:06:09 +0700

    package bottom navigation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,26 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-// ignore: must_be_immutable
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   var faker = Faker();
 
   String date = DateTime.now().toIso8601String();
 
-  HomePage({super.key});
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    print(faker.person.name());
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.green[900],
-        title: const Text(
-          "Package Intl",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: ListView.builder(
+    List<Widget> widgets = [
+      ListView.builder(
         padding: const EdgeInsets.all(20),
         itemCount: 50,
         itemBuilder: (context, index) => ListTile(
@@ -38,6 +36,50 @@ class HomePage extends StatelessWidget {
             DateFormat.yMMMMEEEEd().add_jms().format(DateTime.parse(date)),
           ),
         ),
+      ),
+      const Center(
+        child: Text("Menu 2"),
+      ),
+      const Center(
+        child: Text("Menu 3"),
+      ),
+      const Center(
+        child: Text("Menu 4"),
+      ),
+      const Center(
+        child: Text("Menu 5"),
+      ),
+    ];
+
+    print(faker.person.name());
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.green[900],
+        title: const Text(
+          "Package Bottom Navigation Bar",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: widgets[currentIndex],
+      bottomNavigationBar: ConvexAppBar(
+        backgroundColor: Colors.green[900],
+        color: Colors.white,
+        style: TabStyle.textIn,
+        items: const [
+          TabItem(icon: Icons.home, title: 'Home'),
+          TabItem(icon: Icons.map, title: 'Discovery'),
+          TabItem(icon: Icons.add, title: 'Add'),
+          TabItem(icon: Icons.message, title: 'Message'),
+          TabItem(icon: Icons.people, title: 'Profile'),
+        ],
+        initialActiveIndex: 0,
+        onTap: (int i) {
+          setState(() {
+            currentIndex = i;
+          });
+        },
+        // onTap: (int i) => print('click index=$i'),
       ),
     );
   }
