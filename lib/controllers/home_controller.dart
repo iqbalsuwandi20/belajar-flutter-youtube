fb2305bd9426d1e934da1c9cedb41253708f5278
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-08-06 14:44:13 +0700

    workers

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,72 +1,24 @@
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  RxInt dataInt = 0.obs;
-  RxString dataString = "data".obs;
-  RxDouble dataDouble = 0.0.obs;
-  RxBool dataBoolean = false.obs;
-  RxList<int> dataList = [1, 2, 3].obs;
-  RxSet<int> dataSet = {1, 2, 3}.obs;
-  RxMap<String, dynamic> dataMap = {
-    "id": 1,
-    "name": "Raul",
-    "age": 17,
-  }.obs;
+  RxInt dataValue = 0.obs;
 
-  void increment() {
-    dataInt++;
-  }
+  void change() => dataValue++;
 
-  void decrement() {
-    dataInt--;
-  }
-
-  void updateDataString() {
-    dataString.value = "data (update)";
-  }
-
-  void resetDataString() {
-    dataString.value = "data";
-  }
-
-  void incrementDouble() {
-    dataDouble.value++;
-  }
-
-  void decrementDouble() {
-    dataDouble.value--;
-  }
-
-  void changeBool() {
-    dataBoolean.toggle();
-    // if (dataBoolean.isTrue) {
-    //   dataBoolean.value = false;
-    // } else {
-    //   dataBoolean.value = true;
-    // }
-  }
-
-  int nextValue = 4;
-
-  void addListValue() {
-    dataList.add(nextValue);
-    nextValue++;
-  }
-
-  void changeListValue() {
-    dataList[0] = 99;
-  }
-
-  void addSetValue() {
-    dataSet.add(nextValue);
-    nextValue++;
-  }
-
-  void changeName() {
-    dataMap["name"] = "Iqbal";
-  }
-
-  void resetDataMap() {
-    dataMap["name"] = "Raul";
+  @override
+  void onInit() {
+    // ever(dataValue, (callback) => print("changes"));
+    // once(dataValue, (callback) => print("one changes"));
+    // debounce(
+    //   dataValue,
+    //   (callback) => print("execution 2 seconds after stop monitoring"),
+    //   time: const Duration(seconds: 2),
+    // );
+    interval(
+      dataValue,
+      (callback) => print("execution 2 seconds"),
+      time: const Duration(seconds: 2),
+    );
+    super.onInit();
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,321 +14,35 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blue[900],
         title: const Text(
-          "Tipe data RX GetX",
+          "Workers GetX",
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: ListView(
+      body: Padding(
         padding: const EdgeInsets.all(20),
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Obx(
                 () => Text(
-                  "${homeController.dataInt}",
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green[900]),
-                    onPressed: () {
-                      homeController.increment();
-                    },
-                    child: const Text(
-                      "Add value",
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red[900]),
-                    onPressed: () {
-                      homeController.decrement();
-                    },
-                    child: const Text(
-                      "Less value",
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Obx(
-                () => Text(
-                  "${homeController.dataString}",
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green[900]),
-                    onPressed: () {
-                      homeController.updateDataString();
-                    },
-                    child: const Text(
-                      "Update",
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red[900]),
-                    onPressed: () {
-                      homeController.resetDataString();
-                    },
-                    child: const Text(
-                      "Reset",
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Obx(
-                () => Text(
-                  "${homeController.dataDouble}",
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green[900]),
-                    onPressed: () {
-                      homeController.incrementDouble();
-                    },
-                    child: const Text(
-                      "Add Double",
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red[900]),
-                    onPressed: () {
-                      homeController.decrementDouble();
-                    },
-                    child: const Text(
-                      "Less Duble",
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Obx(
-                () => Text(
-                  "${homeController.dataBoolean}",
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.red[900]),
-                onPressed: () {
-                  homeController.changeBool();
-                },
-                child: const Text(
-                  "Change boolean",
-                  style: TextStyle(
-                    fontStyle: FontStyle.italic,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Obx(
-                () => Text(
-                  "${homeController.dataList}",
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green[900]),
-                    onPressed: () {
-                      homeController.addListValue();
-                    },
-                    child: const Text(
-                      "Add list",
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red[900]),
-                    onPressed: () {
-                      homeController.changeListValue();
-                    },
-                    child: const Text(
-                      "Change list",
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Obx(
-                () => Text(
-                  "${homeController.dataSet}",
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green[900]),
-                    onPressed: () {
-                      homeController.addSetValue();
-                    },
-                    child: const Text(
-                      "Add Set",
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Divider(
-            thickness: 10,
-            color: Colors.pink[900],
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Obx(
-                  () => ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.yellow[900],
-                      child: Text(
-                        "${homeController.dataMap["id"]}",
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    title: Text("${homeController.dataMap["name"]}"),
-                    subtitle: Text("${homeController.dataMap["age"]} year"),
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.grey[900]),
-                onPressed: () {
-                  homeController.changeName();
-                },
-                child: const Text(
-                  "Change",
-                  style: TextStyle(color: Colors.white),
+                  "Changes: ${homeController.dataValue} times",
+                  style: const TextStyle(fontSize: 20),
                 ),
               ),
               const SizedBox(
-                width: 10,
+                height: 20,
               ),
-              ElevatedButton(
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.grey[900]),
-                onPressed: () {
-                  homeController.resetDataMap();
-                },
-                child: const Text(
-                  "Reset",
-                  style: TextStyle(color: Colors.white),
+              TextField(
+                onChanged: (value) => homeController.change,
+                decoration: const InputDecoration(
+                  labelText: "Input your data",
+                  border: OutlineInputBorder(),
                 ),
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
